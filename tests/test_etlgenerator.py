c0bec9210ab70b34139c4442fe61cd49bae9ff64
Tristan <tristan@ai-guru.de>
2018-10-23 15:59:29 +0000

    Progress with ETL-Datagenerator.

--- tests/test_etlgenerator.py
+++ tests/test_etlgenerator.py
@@ -13,21 +13,20 @@ class TestETLGenerator(unittest.TestCase):
 
         dataset_parameters_pointclouds = {}
         dataset_parameters_pointclouds["input_type"] = "pointcloud"
-        dataset_parameters_pointclouds["output_targets"] = ["height", "weight"]
+        #dataset_parameters_pointclouds["output_targets"] = ["height", "weight"]
         dataset_parameters_pointclouds["random_seed"] = 666
         dataset_parameters_pointclouds["pointcloud_target_size"] = 30000
         dataset_parameters_pointclouds["pointcloud_random_rotation"] = True
-        dataset_parameters_pointclouds["dataset_size_train"] = 1000
-        dataset_parameters_pointclouds["dataset_size_test"] = 20
+        #dataset_parameters_pointclouds["dataset_size_train"] = 1000
+        #dataset_parameters_pointclouds["dataset_size_test"] = 20
 
         data_generator = create_datagenerator_from_parameters(dataset_path, dataset_parameters_pointclouds)
 
-        #print("\n".join(data_generator.qrcodes) + "\n{} QR-codes.".format(len(data_generator.qrcodes)))
-
         data_generator.analyze_files()
 
-        #dataset = next(data_generator.generate(size=1, yield_file_paths=True, verbose=True))
-        #assert dataset[0].shape == (1, 30000, 4)
+        dataset = next(data_generator.generate(size=1, yield_file_paths=True, verbose=True))
+        assert dataset[0].shape == (1, 30000, 4), str(dataset[0].shape)
+        assert dataset[1].shape == (1, 1), str(dataset[1].shape)
 
 
 if __name__ == '__main__':
